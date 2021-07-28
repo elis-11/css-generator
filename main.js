31cef82b68bd7f3f345eefaf463f645be32e0d82
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-07-28 21:10:46 +0200

    some new changes

---
+++ main.js
@@ -0,0 +1,83 @@
+console.log("-1-");
+console.log("helloshki!");
+console.log(true);
+console.log(54673);
+console.log("-2-");
+
+const less = "uchim js";
+console.log(less);
+const birthDate = 1990;
+console.log(birthDate);
+let userName = "John";
+console.log(userName);
+userName = "Bill";
+console.log(userName);
+const isMarried = true;
+console.log(isMarried);
+console.log("-3-");
+
+let x = 10;
+let y = 7;
+let result = x + y;
+console.log(result);
+//oder
+console.log(x + y);
+let z = x * 2;
+console.log(x + y + z);
+console.log("-4-");
+
+let greeting = "Hello, ";
+let fName = "John!";
+let result1 = greeting + fName;
+console.log(result1);
+console.log("-5-");
+
+let a = "300";
+let b = "62";
+console.log(a + b);
+console.log("-6-");
+
+let mName = "Alex!";
+let greeting2 = `Hi ${mName}! How are you?`;
+console.log(greeting2);
+console.log("-7-");
+
+let s = 16;
+let d = 5;
+if (s > d) {
+  console.log("s > d");
+} else if (s === d) {
+  console.log("s = d");
+} else {
+  console.log("s < d");
+}
+console.log("-8-");
+const mark = 5;
+switch (mark) {
+  case 1:
+    console.log("Its bud, you have 1");
+    break;
+  case 2:
+    console.log("Its bud toolbar, you have 2");
+    break;
+  case 3:
+    console.log('Its ok, you have 3');
+    break;
+    case 4:
+      console.log('Its gud, you have 4');
+      break
+      case 5:
+        console.log('Very gud, you have 5');
+        break;
+        default:
+          console.log('there is no such assessment');
+}
+
+console.log("-9-");
+const fruits
+
+
+
+console.log("--");
+console.log("--");
+console.log("--");
