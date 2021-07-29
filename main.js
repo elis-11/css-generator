8e05895805f77cc7381a581b889dce716648a075
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-07-29 21:38:47 +0200

    const total = document.getElementById("totalResult")

---
+++ main.js
@@ -0,0 +1,19 @@
+// Inputs Ranges
+const rangeTl = document.getElementById("tlr");
+const rangeTr = document.getElementById("trr");
+const rangeBl = document.getElementById("blr");
+const rangeBr = document.getElementById("brr");
+
+// // Inputs Results
+const resultTl = document.getElementById("result-tlr");
+const resultTr = document.getElementById("result-trr");
+const resultBl = document.getElementById("result-blr");
+const resultBr = document.getElementById("result-brr");
+
+const input = document.querySelectorAll(".input");
+// console.log(input);
+const cube=document.getElementById("cube");
+
+const total = document.getElementById("totalResult")
+const btn= document.getElementById("btn")
+
