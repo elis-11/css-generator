23f8777658f266a4a7003e8070c7ee46a04f5fde
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-07-29 22:25:01 +0200

    btn.addEventListener('click', totalResult);

--- main.js
+++ main.js
@@ -1,19 +1,53 @@
 // Inputs Ranges
-const rangeTl = document.getElementById("tlr");
-const rangeTr = document.getElementById("trr");
-const rangeBl = document.getElementById("blr");
-const rangeBr = document.getElementById("brr");
+const rangeTl = document.getElementById("tlr"),
+  rangeTr = document.getElementById("trr"),
+  rangeBl = document.getElementById("blr"),
+  rangeBr = document.getElementById("brr");
 
 // // Inputs Results
-const resultTl = document.getElementById("result-tlr");
-const resultTr = document.getElementById("result-trr");
-const resultBl = document.getElementById("result-blr");
-const resultBr = document.getElementById("result-brr");
+const resultTl = document.getElementById("result-tlr"),
+  resultTr = document.getElementById("result-trr"),
+  resultBl = document.getElementById("result-blr"),
+  resultBr = document.getElementById("result-brr");
 
 const input = document.querySelectorAll(".input");
 // console.log(input);
-const cube=document.getElementById("cube");
+const cube = document.getElementById("cube");
 
-const total = document.getElementById("totalResult")
-const btn= document.getElementById("btn")
+const total = document.getElementById("totalResult");
+const btn = document.getElementById("btn");
 
+const changeRadius = () => {
+  resultTl.innerHTML = rangeTl.value;
+  resultTr.innerHTML = rangeTr.value;
+  resultBl.innerHTML = rangeBl.value;
+  resultBr.innerHTML = rangeBr.value;
+
+  cube.style.borderRadius =
+    rangeTl.value +
+    "px " +
+    rangeTr.value +
+    "px " +
+    rangeBr.value +
+    "px " +
+    rangeBl.value +
+    "px";
+};
+
+const totalResult = () => {
+  total.innerHTML =
+    rangeTl.value +
+    " " +
+    rangeTr.value +
+    " " +
+    rangeBr.value +
+    " " +
+    rangeBl.value +
+    " ";
+};
+
+for (node of input) {
+  node.addEventListener("input", changeRadius);
+}
+
+btn.addEventListener('click', totalResult);
